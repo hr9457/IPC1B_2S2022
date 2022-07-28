52c2455349510b84fe47dc10b40becbf32e20f68
hector <hr9457@gmail.com>
2022-07-28 13:21:49 -0600

    repositorio oficial del laboratorio

---
+++ PrimerPrograma/src/primerprograma/PrimerPrograma.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package primerprograma;
+
+/**
+ *
+ * @author joshu
+ */
+public class PrimerPrograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int contador = 0;
+        
+//        true 0 false
+        System.out.println(contador >= 5);
+        
+        while(contador <= 5){
+            // ejecuto mi hola mundo            
+            System.out.println("Hola mundo");
+            
+            // le sumo en uno a la variable
+            contador = contador + 1;
+        }
+        
+        System.out.println("fin");
+        System.out.println("adios");
+    }
+    
+}
